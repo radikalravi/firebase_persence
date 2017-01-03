5f256e9d3af1858629591ff389abbe72992f3470
Ravi S. Singh <ravi@radikal-labs.com>
2017-01-03 13:35:08 +0530

    firebase confing change by govind

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var Firebase = require('firebase');
 var bodyParser = require('body-parser');
-var port= 3000;
+var port= process.env.PORT ||3000;
 
 var app = express();
 
 var config = {
-	apiKey: "AIzaSyBILjsq_QSbE8X-H2tiSxJxX5i1AV139nI",
-    authDomain: "monopoly-2ffc6.firebaseapp.com",
-    databaseURL: "https://monopoly-2ffc6.firebaseio.com",
-    storageBucket: "monopoly-2ffc6.appspot.com",
-    messagingSenderId: "68477236556"
+	apiKey: "AIzaSyBTl501aneuEHtxky5_g6Etl_atYxPM80s",
+    authDomain: "fir-test-6468b.firebaseapp.com",
+    databaseURL: "https://fir-test-6468b.firebaseio.com",
+    storageBucket: "fir-test-6468b.appspot.com",
+    messagingSenderId: "439463118427"
 };
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
